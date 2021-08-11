426939d576f2d0f4c0c0043bc0da9739011cafb4
Bruno Eneas Costa <brunoco56@hotmail.com>
2021-08-11 13:47:31 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StandBy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
