b9356b6922580b0e57589891c8d8f98466a1084d
Bruno Eneas Costa <brunoco56@hotmail.com>
2021-08-12 10:59:08 -0300

    criação do ClienteViewModel

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,14 @@
+﻿using Services.Services;
+
+namespace StandBy.Controllers
+{
+    public class ClienteController : BaseService<Cliente>
+    {
+        private IBaseService<Cliente> _baseUserService;
+
+        public ClienteController(IBaseService<Cliente> baseUserService)
+        {
+            _baseUserService = baseUserService;
+        }
+    }
+}
---
+++ Models/ClienteViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StandBy.Models
+{
+    public class ClienteViewModel
+    {
+        public string ClienteId { get; set; }
+        public string Razao_Social { get; set; }
+        public string Cnpj { get; set; }
+        public DateTime Data_Fundacao { get; set; }
+        public decimal Capital { get; set; }
+        public bool Quarentena { get; set; }
+        public bool Status_Cliente { get; set; }
+
+        public string Classificacao { get; set; }
+
+    }
+}
